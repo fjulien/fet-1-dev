541a116a64c1082a2cc6556a1504a20aacc67d0c
fjulien <fettingerjulien@yahoo.fr>
2023-01-23 07:58:34 +0000

    save

--- components/Codesandbox.tsx
+++ components/Codesandbox.tsx
@@ -1,19 +1,30 @@
 import { CSSProperties, FC } from "react";
 
-type Props = {
-  src?: string,
-  title?: string,
-  allow?: string,
-  sandbox?: string
-}
+type Props =  Partial<{
+  src: string,
+  title: string,
+  allow: string,
+  sandbox: string,
+  paramsUrl?: {
+    path:string,
+    view: 'editor' 
+  }
+}>
 
 const Codesandbox: FC<Props> = (props) => {
   const {
-    src = "/hello-world-b5cpwn?fontsize=14&hidenavigation=1&theme=dark",
+    src,
     title,
     allow = "accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking",
-    sandbox = "allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
+    sandbox = "allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts",
+    paramsUrl
   } = props;
+
+  
+  const view = paramsUrl?.view || 'editor';
+  const path = paramsUrl?.path || 'index.html';
+
+
   const style: CSSProperties = {
     width: '100%',
     height: '500px', 
@@ -23,7 +34,7 @@ const Codesandbox: FC<Props> = (props) => {
   }
   return (
     <iframe
-      src={`https://codesandbox.io/embed${src}&fontsize=14&hidenavigation=1&theme=dark&view=editor`}
+      src={`https://codesandbox.io/embed${src}&fontsize=14&hidenavigation=1&theme=dark&view=${view}&module=${path}`}
       style={style}
       title={title}
       allow={allow}
