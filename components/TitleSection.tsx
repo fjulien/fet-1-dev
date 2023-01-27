d4777f0f1476843eadb2fc1f309a279dadd320f6
fjulien <fettingerjulien@yahoo.fr>
2023-01-27 20:39:59 +0000

    save

---
+++ components/TitleSection.tsx
@@ -0,0 +1,17 @@
+import { FC } from 'react'
+
+type Props = {
+  title: string
+  children?: React.ReactNode
+}
+
+const TitleSection: FC<Props> = ({ title, children }) => {
+  const url = title.replaceAll(' ', '-').toLowerCase()
+  return (
+    <h2 id={url}>
+      <a href={`#${url}`}>#</a> {title} {children}
+    </h2>
+  )
+}
+
+export default TitleSection
