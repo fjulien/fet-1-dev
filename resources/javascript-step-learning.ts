cc2fc67f75081820cca61846bc759240b92f8d74
fjulien <39367571+fjulien@users.noreply.github.com>
2023-02-27 14:57:21 +0000

    save

---
+++ resources/javascript-step-learning.ts
@@ -0,0 +1,27 @@
+import type { DetailsType } from '../types';
+
+export const javascriptStepLearning: DetailsType = {
+  title: 'Level 1',
+  contents: [
+    {
+      title: 'Desciption rapide de JavaScript',
+      contents: ['']
+    },
+    {
+      title: 'HTML',
+      contents: ['']
+    },
+    {
+      title: 'Les variables',
+      contents: ['']
+    },
+    {
+      title: 'Les fonctions',
+      contents: ['']
+    },
+    {
+      title: 'Projet',
+      contents: ['Compteur']
+    }
+  ]
+};
